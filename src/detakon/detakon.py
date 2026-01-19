1c03b22a5fecdb0e62245be83b84e45298af4678
Clef Knightfury <clef@knightfury.org>
2026-01-18 19:03:48 -0800

    initial commit

---
+++ src/detakon/detakon.py
@@ -0,0 +1,18 @@
+class Detakon():
+    """detakon uses a detakon map to convert data."""
+    def __init__(self, detamap, source, destination, stype: str="csv", dtype: str="csv", *args, **kargs):
+        """
+        Docstring for __init__
+        
+        :param self: self object.
+        :param detamap: detamap location that describes field mappings, default values, and operations to perform on data.
+        :param source: Source to input.
+        :param destination: Destination to output.
+        :param stype: Source data format type. Defaults to CSV.
+        :type stype: str
+        :param dtype: Destination data format type. Defaults to CSV.
+        :type dtype: str
+        :param args: Additional parameters.
+        :param kargs: Addtional flags.
+        """
+        pass
