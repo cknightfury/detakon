e705961852c7dd83f89fe948630bcf73deccc9c8
Clef Knightfury <clef@knightfury.org>
2026-02-27 21:44:48 -0800

    Minimal viable product release for CSV to CSV conversion

--- README.md
+++ README.md
@@ -12,11 +12,69 @@
 
 ## Installation
 
-NOTE: Currently in pre-release.  Not for actual usage.
+NOTE: Currently in pre-release.  Current status is: Minimal viable product for CSV to CSV conversions only.  Not for usage in production systems.
 
 ```console
 pip install detakon
 ```
+
+## Detamap Files
+
+A detamap configuration file is used to provide all details necessary for the data conversion.
+
+A datamap must be a Python dictionary, or convertable to a Python dictionary.  Currently this means either a dictionary or JSON file (plans to add TOML support).
+
+A detamap MUST include the following key:value pairs:
+- "Fields" as a list of strings corresponding to the output field names in the desired order.
+- "Mappings" with a sub-dictionary of source data field names as keys, mapped to output field names as values.
+- "Defaults" with a sub-dictionary of output field names that will supply a default value if either: (not currently implemented)
+    - Matching field was not found in source.
+    - Matching source data was empty, such as an empty string.
+- "Operations" with a sub-dictionary of operations to perform on output data: (not currently implemented)
+    - Example opersations may be "upper", or "convertValue".
+- "Source" with a sub-dictionary that defines the type of source being passed, and the arguments to pass to Path.open() if source is a file, or csv.DictReader if source is CSV data.
+    - Required keys:
+        - "argument" as type of argument being passed as the source argument to the Detakon initializer. Accepted values: "filepath".
+            - "type" key may be required depending on the argument being passed.
+- "Output" with a sub-dictionary that defines the type of output expected, and the arguments to pass to Path.open() if destination is a file, or csv.DictReader if output is CSV data. 
+
+A detamap JSON file may look similar to:
+
+```
+{
+    "Fields": ["External Order ID", "Last Name", "First Name", "DOB"],
+    "Mappings": {
+        "Invoice #": "External Order ID",
+        "Client Last Name": "Last Name",
+        "Client First Name": "First Name",
+        "Date of Birth": "DOB"
+    },
+    "Defaults": {
+        "DOB": "Unknown"
+    },
+    "Operations": {
+        "upper": ["External Order ID", "Last Name", "First Name"]
+    },
+    "Source": {
+        "argument": "filepath",
+        "type": "str",
+        "encoding": "utf-8",
+        "format": "csv",
+        "delimiter": ","
+    },
+    "Output": {
+        "argument": "filepath",
+        "type": "str",
+        "append": false,
+        "omit_heading": false,
+        "encoding": "utf-8",
+        "format": "csv",
+        "delimiter": ","
+    }
+}
+```
+
+Above sample is not all-inclusive.  Detamaps, source data, and output expect full chain of custody.  No security guarantees are made in regards to untrusted or malformed data.
 
 ## License
 
--- src/detakon/detakon.py
+++ src/detakon/detakon.py
@@ -2,10 +2,11 @@ import json
 from pathlib import Path
 from io import IOBase, TextIOWrapper
 from types import GeneratorType
+from csv import DictReader, DictWriter
 
 class Detakon():
     """detakon uses a detakon map to convert data."""
-    def __init__(self, detamap, source, destination, sdtype: str="csv", ddtype: str="csv", *args, **kargs):
+    def __init__(self, detamap, source, destination, *args, **kargs):
         """
         Initialize all values for Detakon object.
         
@@ -13,17 +14,64 @@ class Detakon():
         :param detamap: detamap location that describes field mappings, default values, and operations to perform on data.
         :param source: Source to input.  See load_source method for accepted types.
         :param destination: Destination to output.
-        :param sdtype: Source data format type. Defaults to CSV.
-        :type sdtype: str
-        :param ddtype: Destination data format type. Defaults to CSV.
-        :type ddtype: str
         :param args: Additional parameters.
         :param kargs: Addtional flags.
         """
-        self.detamap = self.load_detamap(detamap)
-        self.source = self.load_source(source) # see note in method pass
+        self.detamap: dict = self.load_detamap(detamap)
+        self.mappings: dict = self.detamap["Mappings"]
+        self.source = source
+        self.destination = destination
+        data_generator = self.source_reader()
+        output_info: dict = self.detamap["Output"]
+        output = output_info["argument"]
 
-    def load_source(self, source) -> str:
+        # branch to determine output method called based on detamap.Output.argument for destination parameter
+        # filepath as either string or Path object 
+        if output == "filepath":
+            if output_info["type"] == "str":
+                self.destination = Path(self.destination)
+
+            # if not appending to existing file, delete if file exists and create new empty file
+            if not output_info.get("append", False) and self.destination.exists() and self.destination.is_file():
+                self.destination.unlink()
+
+            if self.destination.exists() and self.destination.is_file():
+                new_file = False
+            else:
+                new_file = True
+            self.destination.touch()
+
+            with self.destination.open(mode="a",
+                        buffering=output_info.get("buffering", -1),
+                        encoding=output_info.get("encoding", "utf-8"),
+                        errors=output_info.get("errors", None),
+                        newline=output_info.get("newline", None)) as file:
+                csv_writer = DictWriter(file,
+                            fieldnames=self.detamap["Fields"],
+                            # restkey=output_info.get("restkey", None),
+                            restval=output_info.get("restval", ""),
+                            extrasaction=output_info.get("extrasaction", "raise"),
+                            dialect=output_info.get("dialect", "excel"),
+                            delimiter=output_info.get("delimiter", ","),
+                            quotechar=output_info.get("quotechar", '"'),
+                            escapechar=output_info.get("escapechar", None),
+                            doublequote=output_info.get("doublequote", True),
+                            skipinitialspace=output_info.get("skipinitialspace", False),
+                            lineterminator=output_info.get("lineterminator", "\r\n"),
+                            quoting=output_info.get("quoting", 0),
+                            strict=output_info.get("strict", False))
+                
+                if new_file and not output_info.get("omit_heading", False):
+                    csv_writer.writeheader()
+                for entry in data_generator:
+                    row_data = {}
+                    for source_field, destination_field in self.mappings.items():
+                        row_data[destination_field] = entry[source_field]
+                    csv_writer.writerow(row_data)
+        elif output == "return":
+            pass
+
+    def source_reader(self):
         """
         Validate source type, and return a generator object if possible, otherwise return full object in accepted format.
 
@@ -34,17 +82,54 @@ class Detakon():
         :return: File path as string.
         :rtype: str
         """
-        # Thoughts: Accept file or generator object reference - rely on caller to supply data stream if not a file.
-        # Halted development - reconsideration for how to additionally accept non-file streams
-        if isinstance (source, Path):
-            pass
-        elif isinstance(source, TextIOWrapper):
-            pass
-        elif isinstance(source, str):
-            if Path(source).exists() and Path(source).is_file():
-                pass
-        else:
-            pass
+        # get source information from detamap
+        source_info: dict = self.detamap["Source"]
+
+        # branch based on source.argument value provided in detamap
+
+        # handler for source.argument being a filepath
+        # filepath must be either a string to a file, or a Path object for a file.
+        if source_info["argument"] == "filepath":
+            if source_info["type"] == "str":
+                self.source = Path(self.source)
+            if self.source.exists() and self.source.is_file():
+                with self.source.open(mode='r',
+                                        buffering=source_info.get("buffering", -1),
+                                        encoding=source_info.get("encoding", "utf-8"),
+                                        errors=source_info.get("errors", None),
+                                        newline=source_info.get("newline", None)) as source_file:
+                    # handler for source.format csv values.  if no source.format value is provided, csv value is default
+                    if source_info.get("format", "csv") == "csv":
+                        # DictReader defaults are used if Source does not contain a key for a given keyword.
+                        # key:value paris can be provided in the detamap Source section to set the value of any DictReader keyword.
+                        # values must conform with DictReader's expectation.
+                        csv_reader = DictReader(source_file,
+                                                fieldnames=source_info.get("fieldnames", None),
+                                                restkey=source_info.get("restkey", None),
+                                                restval=source_info.get("restval", None),
+                                                dialect=source_info.get("dialect", "excel"),
+                                                delimiter=source_info.get("separator", ","),
+                                                quotechar=source_info.get("quotechar", '"'),
+                                                escapechar=source_info.get("escapechar", None),
+                                                doublequote=source_info.get("doublequote", True),
+                                                skipinitialspace=source_info.get("skipinitialspace", False),
+                                                lineterminator=source_info.get("lineterminator", "\r\n"),
+                                                quoting=source_info.get("quoting", 0),
+                                                strict=source_info.get("strict", False))
+                        for row in csv_reader:
+                            yield row
+
+        # elif isinstance(source, TextIOWrapper):
+        #     pass
+        # elif isinstance(source, str):
+        #     if Path(source).exists() and Path(source).is_file():
+        #         pass
+        #     elif isinstance(source, dict):
+        #         pass
+        # elif isinstance(source, GeneratorType):
+        #     pass
+        # else:
+        #     raise ValueError("Source could not be determined.")
 
     def load_detamap(self, detamap) -> dict:
         """
@@ -55,6 +140,7 @@ class Detakon():
         :return: Dictionary of detamap
         :rtype: dict
         """
+        # accepts python dictionary or JSON data.  Future plans to add TOML detamap.
         if isinstance(detamap, dict):
             return detamap
         elif isinstance(detamap, str) and detamap[0] == '{':
