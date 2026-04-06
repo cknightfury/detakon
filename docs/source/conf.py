04f414f3ea6283b48d2050fe3a10976391acb48b
Clef Knightfury <clef@knightfury.org>
2026-04-06 08:08:50 -0700

    updated docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,14 +24,14 @@ release = __about__.__version__
 extensions = ['sphinx.ext.autodoc', 'myst_parser']
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['build']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 source_suffix = {
